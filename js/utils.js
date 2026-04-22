43f668c76cec6d02825269a2c443ec19d8852a07
Julie <juliethron@gmail.com>
2026-04-22 19:16:07 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,12 @@
+export function saveToken(token) {
+  localStorage.setItem("token", token);
+}
+
+export function getToken() {
+  return localStorage.getItem("token");
+}
+
+export function clearToken() {
+  localStorage.removeItem("token");
+}
 
