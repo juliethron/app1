b672c35f7bb26029d5cdf9db3d560ab1352824ec
Julie <juliethron@gmail.com>
2026-04-26 20:35:55 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.noroff.dev/api/v1/social";
+const API_URL = "https://api.noroff.dev/api/v2";
 
 export async function registerUser(name, email, password) {
   const response = await fetch(`${API_URL}/auth/register`, {
